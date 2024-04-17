d927bc8300184d388d6f546957b71f767915e9c6
asmitaskamble <116185261+asmitaskamble@users.noreply.github.com>
2024-04-17 20:04:17 +1000

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,24 +1,46 @@
 import streamlit as st
-import pandas as pd
-# Importing tab function from tab.py
+import base64
 from tab import create_tabs
 
 st.set_page_config(
     page_title="ECOHEALTH WEBPAGE",
     page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
+
+custom_css = """
+<style>
+body {
+    background-color: #ffff99;
+    color: #333;
+}
+.sidebar .sidebar-content {
+    background-color: #ffffff;
+}
+.stButton>button {
+    background-color: #ffcc00 !important;
+}
+</style>
+"""
 
 st.write("# Welcome to WebPage of EcoHealth! ")
 
+st.sidebar.markdown(custom_css, unsafe_allow_html=True)
 st.sidebar.success("Select a Pollutant Below.")
 pollutant = st.sidebar.selectbox(
     "Choose a pollutant:",
     ("Methane", "Ammonia")
 )
 
+st.sidebar.markdown(custom_css, unsafe_allow_html=True)
+st.sidebar.success("Select a cause of air pollution.")
+pollutant = st.sidebar.selectbox(
+    "Choose a cause:",
+    ("Enteric fermentation", "Manure Management", "Agrifood systems waste disposal")
+)
+
 st.markdown("""
-    ## ABOUT ECOHEALTH
-    
     This webpage is built by team EcoHealth.
     This page serves as a comprehensive resource for exploring the profound impact of pollution on global health.
     Through detailed visualizations, information, and curated content, visitors will gain an in-depth understanding of how various pollutants adversely affect the environment and
@@ -30,5 +52,11 @@ st.markdown("""
     Check out [World Health Organization](https://www.who.int)
 """)
 
-# Function call to create tabs
-create_tabs()
+# Select the tab
+selected_tab = st.selectbox(
+    "Select a tab:",
+    ("Visualisation", "Information", "Dataset", "References")
+)
+
+# Call the function to create tabs
+create_tabs(selected_tab)
