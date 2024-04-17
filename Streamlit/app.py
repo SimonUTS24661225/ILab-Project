0c5c0a7d78ab2664fc748916debf83d7acb7465e
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-18 01:27:52 +1000

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,27 +2,26 @@ import streamlit as st
 import base64
 from tab import create_tabs
 
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://cdn.pixabay.com/photo/2020/06/19/22/33/wormhole-5319067_960_720.jpg");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
     
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg("Streamlit/test.png")
-
 st.set_page_config(
     page_title="ECOHEALTH WEBPAGE",
     page_icon="👋",
