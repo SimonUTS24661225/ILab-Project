32125b4c32216ff9ea8e83b57ae91390fdb87405
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-18 01:29:46 +1000

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -60,5 +60,9 @@ selected_tab = st.selectbox(
     ("Visualisation", "Information", "Dataset", "References")
 )
 
+set_bg_hack_url()
+
 # Call the function to create tabs
 create_tabs(selected_tab)
+
+
