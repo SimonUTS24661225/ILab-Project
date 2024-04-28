dc152dce39d76de8613265b359f3b994aa971d3f
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-28 17:30:36 +1000

    Update Agriculture.py

--- Streamlit/Ammonia/Agriculture.py
+++ Streamlit/Ammonia/Agriculture.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# Set page title
 st.title("Agricultural Ammonia Emissions")
 
 # Subtitles
@@ -16,9 +15,12 @@ The application of nitrogen-based fertilizers in agriculture leads to ammonia vo
 
 st.header("Environmental Challenges")
 st.write("""
-These emissions pose environmental challenges, including air pollution and nutrient runoff, which can have adverse effects on ecosystems and human health. Ammonia emissions contribute to the formation of fine particulate matter and can exacerbate respiratory issues. Moreover, ammonia deposition can lead to nutrient imbalances in soil and water bodies, affecting biodiversity and water quality.
+These emissions pose environmental challenges, including air pollution and nutrient runoff, which can have adverse effects on ecosystems and human health. 
+Ammonia emissions contribute to the formation of fine particulate matter and can exacerbate respiratory issues. Moreover, ammonia deposition can lead to nutrient imbalances in soil and water bodies, affecting biodiversity and water quality.
 """)
 
 st.header("Sustainable Agricultural Practices")
 st.write("""
-To address these challenges, sustainable agricultural practices are essential. Strategies such as improving manure management, optimizing fertilizer application techniques, and promoting precision agriculture can help reduce ammonia emissions. Additionally, increasing the adoption of alternative farming methods like organic farming and agroforestry can mitigate ammonia emissions while promoting soil health and biodiversity.
+To address these challenges, sustainable agricultural practices are essential. Strategies such as improving manure management, optimizing fertilizer application techniques, and promoting precision agriculture can help reduce ammonia emissions.
+Additionally, increasing the adoption of alternative farming methods like organic farming and agroforestry can mitigate ammonia emissions while promoting soil health and biodiversity.
+""")
