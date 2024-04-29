02d2ff53be64b1bb4abee2b0494ea359ca02fc19
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-29 17:16:28 +1000

    Update visualization.py

--- Streamlit/Ammonia/visualization.py
+++ Streamlit/Ammonia/visualization.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.image("ammonia1.png", caption="A Graph of Total Ammonia Emission Over years")
+st.image("Streamlit/Ammonia/ammonia1.png", caption="A Graph of Total Ammonia Emission Over years")
 
-st.image("ammonia2.png", caption="Ammonia Emissions All Around the World")
+st.image("Streamlit/Ammonia/ammonia2.png", caption="Ammonia Emissions All Around the World")
 
-st.image("Ammonia3.png", caption="Top Regions and Countries Releasing Ammonia")
+st.image("Streamlit/Ammonia/Ammonia3.png", caption="Top Regions and Countries Releasing Ammonia")
