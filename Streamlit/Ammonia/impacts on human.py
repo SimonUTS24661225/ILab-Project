eb52f171d8bf34eb096455f95e34eb6fb8cd0ed6
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-29 11:17:55 +1000

    Update impacts on human.py

--- Streamlit/Ammonia/impacts on human.py
+++ Streamlit/Ammonia/impacts on human.py
@@ -10,6 +10,7 @@ def health_impacts_of_ammonia_toxicity():
     Ammonia metabolism primarily occurs in the liver. In individuals with liver disease, such as cirrhosis or hepatitis, the liver may be less efficient at processing and detoxifying ammonia. Elevated levels of ammonia in the blood can exacerbate liver damage and contribute to the progression of liver disease.
     """)
     image_url_liv = "https://www.metropolisindia.com/upgrade/blog/upload/23/12/Liver-Failure1703930383.webp"
+    st.image(image_url_liv, caption="Image Caption")
 
 
     # Hepatic Encephalopathy
@@ -17,12 +18,18 @@ def health_impacts_of_ammonia_toxicity():
     st.write("""
     Hepatic encephalopathy is a condition characterized by cognitive dysfunction and neurological symptoms resulting from liver dysfunction and the accumulation of toxic substances, including ammonia, in the bloodstream. High levels of ammonia can impair brain function and contribute to symptoms such as confusion, disorientation, impaired concentration, and personality changes.
     """)
+    image_url_hep = "https://www.alcimed.com/wp-content/uploads/2021/09/encephalopathie-hepatique-1.jpg"
+    st.image(image_url_hep, caption="Image Caption")
 
     # Reye's Syndrome
     st.write("#### Reye's Syndrome:")
     st.write("""
     Reye's syndrome is a rare but serious condition that primarily affects children and teenagers recovering from viral infections, such as influenza or chickenpox. While the exact cause of Reye's syndrome is not fully understood, it has been linked to the use of aspirin during viral illnesses. Ammonia toxicity has been proposed as one of the contributing factors in the development of Reye's syndrome, although the exact mechanism remains unclear.
     """)
+    image_url_rey = "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/300004745_1123123021932847_3742562859581901997_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=XKCWg1tdJAQAb6neU3J&_nc_ht=scontent.fsyd3-1.fna&oh=00_AfDT6zlp9VZLB8SJbHNL-_Z3An-TRlyINTnNZf2o_QqVPQ&oe=6634D50B"
+    st.image(image_url_rey, caption="Image Caption")
+
+    
 
     # Decreased Blood Flow to Liver
     st.write("#### Decreased Blood Flow to Liver:")
