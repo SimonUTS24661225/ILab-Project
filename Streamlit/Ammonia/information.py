dfd592155d58cf4c8397d6fc8a26ae7e7d4a77c1
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-04-29 10:44:47 +1000

    Update information.py

--- Streamlit/Ammonia/information.py
+++ Streamlit/Ammonia/information.py
@@ -12,6 +12,8 @@ Ammonia is a colorless gas with a pungent odor and is commonly used in agricultu
 st.write("#### Major Sources of Ammonia Emissions")
 st.write("""
 - Agricultural activities, including livestock farming and fertilizer application (70%)
+image_url = "https://www.openaccessgovernment.org/wp-content/uploads/2018/07/dreamstime_s_114372506.jpg"
+st.image(image_url, caption="Image Caption")
 - Natural Processing in Ocean (15%)
 - Wildfires (10%)
 - Industrial processes, such as chemical manufacturing and wastewater treatment (1-2%)
@@ -38,8 +40,7 @@ Exposure to high levels of ammonia can lead to various health problems, includin
 Long-term exposure to ammonia may increase the risk of chronic respiratory conditions and exacerbate pre-existing respiratory diseases, such as asthma and chronic obstructive pulmonary disease (COPD). Additionally, ammonia emissions can contribute to air pollution and environmental degradation, affecting both human health and ecosystems.
 """)
 
-image_url = "https://www.openaccessgovernment.org/wp-content/uploads/2018/07/dreamstime_s_114372506.jpg"
-st.image(image_url, caption="Image Caption")
+
 
 st.write("#### Countries of Highest Ammonia Emission")
 st.image("Streamlit/Ammonia/Country-breakdown-of-global-ammonia-production-2021-Source-6.png", caption="2021 Country Breakdown of Global Ammonia Production")
