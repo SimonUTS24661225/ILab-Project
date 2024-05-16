8d287b869c5e1952ee7d47d3918069d972ba3ea1
Simon Lim <116344920+SimonUTS24661225@users.noreply.github.com>
2024-05-16 20:44:49 +1000

    Update visualization.py

--- Streamlit/Ammonia/visualization.py
+++ Streamlit/Ammonia/visualization.py
@@ -1,18 +1,19 @@
 import streamlit as st
 st.write("### A Graph of Total Ammonia Emission Over years")
 st.image("Streamlit/Ammonia/ammonia1.png", caption="A Graph of Total Ammonia Emission Over years")
-st.write("The graph shows that Ammonia emissions are potentially increasing over years due to many 
+st.write("""The graph shows that ammonia emissions are potentially increasing over years due to many 
 environmental and human-triggered factors, including agricultural activities, industrial 
-processes, vehicle emissions, climate change and waste management.")
+processes, vehicle emissions, climate change, and waste management.""")
+
 st.write("### Ammonia Emissions All Around the World")
 st.image("Streamlit/Ammonia/ammonia2.png", caption="Ammonia Emissions All Around the World")
-st.write("The world map shows total ammonia emissions that were released from 1750 to 2020 for each 
+st.write("""The world map shows total ammonia emissions that were released from 1750 to 2020 for each 
 country. According to the visual world map, China has produced the highest amount of ammonia 
-emissions, followed by India (524,821,539), USA (263,041,665) and Russia (211,097,777). It 
-appears that most of ammonia emissions have been produced in Asia and upper income 
-countries.")
+emissions, followed by India (524,821,539), USA (263,041,665), and Russia (211,097,777). It 
+appears that most of the ammonia emissions have been produced in Asia and upper income 
+countries.""")
 st.write("### Top Regions and Countries Releasing Ammonia")
 st.image("Streamlit/Ammonia/Ammonia3.png", caption="Top Regions and Countries Releasing Ammonia")
-st.write("The table confirms that ammonia emissions are more frequently produced in Asia and Upper
-middle income countries.")
+st.write("""The table confirms that ammonia emissions are more frequently produced in Asia and upper
+middle income countries.""")
 
